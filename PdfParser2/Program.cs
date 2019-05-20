1e103c0a9ac05851eb8afcb172fdb5784e91291d
SebMuir-Smith <sebastian@muirsmith.com>
2019-05-20 15:48:22 +1000

    Add safe pdf checking and code efficiency improvements

--- PdfParser2/Program.cs
+++ PdfParser2/Program.cs
@@ -20,34 +20,54 @@ namespace PdfParser2
                 return;
             }
             
-            // Path of pdf folder, relative from ~/
+            // Path of pdf folder, relative from where it is being run from
             string path = args[0];
 
             // Regex to search for 
             string expression = args[1];
             
             Regex regex = new Regex(expression);
-            
+
+            PdfPage page;
+            string pageText;
+            string filePath;
+            string fileName;
             // For each pdf in folder
             for (int docN = 0; docN < Directory.GetFiles(path).Length; docN++)
             {
-                // Open pdf
-                using (PdfDocument pdf = new PdfDocument(Directory.GetFiles(path)[docN]))
+                filePath = Directory.GetFiles(path)[docN];
+                fileName = Path.GetFileName(filePath);
+                
+                // Skip any hidden files
+                if (fileName[0] == '.')
                 {
-                    // For each page in pdf
-                    for (int pageN = 0; pageN < pdf.PageCount; pageN++)
+                    continue;
+                }
+                
+                // Open pdf safely
+                try
+                {
+                    using (PdfDocument pdf = new PdfDocument(fileName))
                     {
-                        PdfPage page = pdf.GetPage(pageN);
-
-                        string pageText = page.GetText();
-
-                        // For each match on page
-                        foreach (Match match in regex.Matches(pageText))
+                        // For each page in pdf
+                        for (int pageN = 0; pageN < pdf.PageCount; pageN++)
                         {
-                            Console.WriteLine("Word was found on page {0} in document {1}, ({2})", 
-                                pageN +1, docN + 1, Directory.GetFiles(path)[docN]);
+                            page = pdf.GetPage(pageN);
+
+                            pageText = page.GetText();
+
+                            // For each match on page
+                            foreach (Match match in regex.Matches(pageText))
+                            {
+                                Console.WriteLine("Word was found on page {0} in document {1}, ({2})",
+                                    pageN + 1, docN + 1, fileName);
+                            }
                         }
                     }
+                }
+                catch (BitMiracle.Docotic.Pdf.UnexpectedStructureException)
+                {
+                    Console.WriteLine("Sorry, document {0} ({1}) was not recognised", docN + 1, fileName);
                 }
             }           
         }
